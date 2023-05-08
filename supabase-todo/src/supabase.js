c0be88495cc8caef06dbce8bbad22a05591af93d
Gaurav Roy <111721656+gauravroy21@users.noreply.github.com>
2023-05-08 16:32:40 +0530

    Update supabase.js

--- supabase-todo/src/supabase.js
+++ supabase-todo/src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://rhwjnekoafrqcbbwrkso.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJod2puZWtvYWZycWNiYndya3NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkxOTUyMjYsImV4cCI6MTk5NDc3MTIyNn0.J4v5V94o70Pg9hiSGC1FFe9LySWsMbASLlxdGyvDbC0";
+const supabaseUrl = "";
+const supabaseAnonKey = "";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 export default supabase;
